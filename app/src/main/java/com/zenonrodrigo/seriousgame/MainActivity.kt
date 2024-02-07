e522abadcf5e6388274b702be79aee1fa0b8b9b8
Zenon <zenropru@gmail.com>
2024-02-07 20:59:37 +0100

    nivell 3 casi fet

--- app/src/main/java/com/zenonrodrigo/seriousgame/MainActivity.kt
+++ app/src/main/java/com/zenonrodrigo/seriousgame/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.zenonrodrigo.seriousgame
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Puedes cargar la vista de inicio o iniciar otra actividad aquí
+        // Por ejemplo, si tienes una pantalla de inicio, podrías hacer algo así:
+        val intent = Intent(this, Nivell3::class.java)
+        startActivity(intent)
+        finish() // Termina la actividad actual para evitar volver atrás a ella
     }
 }
