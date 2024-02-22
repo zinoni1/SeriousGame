d977963c7d8f9f808c2ab1674781333598a4191e
Zenon <zenropru@gmail.com>
2024-02-22 20:51:57 +0100

    nivell3 i 7 fets

--- app/src/main/java/com/zenonrodrigo/seriousgame/MainActivity.kt
+++ app/src/main/java/com/zenonrodrigo/seriousgame/MainActivity.kt
@@ -1,65 +1,72 @@
 package com.zenonrodrigo.seriousgame
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.card.MaterialCardView
+import com.zenonrodrigo.seriousgame.sopadelletres.Sopa
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var draggableCard1: MaterialCardView
-    private lateinit var draggableCard2: MaterialCardView
-    private lateinit var draggableCard3: MaterialCardView
-    private lateinit var draggableCard4: MaterialCardView
-    private lateinit var parentContainer: DraggableCoordinatorLayout
+    //private lateinit var draggableCard1: MaterialCardView
+    //private lateinit var draggableCard2: MaterialCardView
+    //private lateinit var draggableCard3: MaterialCardView
+    //private lateinit var draggableCard4: MaterialCardView
+    //private lateinit var parentContainer: DraggableCoordinatorLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Puedes cargar la vista de inicio o iniciar otra actividad aquí
         // Por ejemplo, si tienes una pantalla de inicio, podrías hacer algo así:
-        val intent = Intent(this, Nivell3::class.java)
-        startActivity(intent)
-        finish() // Termina la actividad actual para evitar volver atrás a ella
         setContentView(R.layout.activity_main)
 
+        //boton 3 intent al pulsar
+        val boton3 = findViewById<View>(R.id.lvl3)
+        val boton7 = findViewById<View>(R.id.lvl7)
+        boton3.setOnClickListener {
+            val intent = Intent(this, Nivell3::class.java)
+            startActivity(intent)
+        }
+        boton7.setOnClickListener {
+            val intent = Intent(this, Sopa::class.java)
+            startActivity(intent)
+        }
+
         // Inicializar las vistas
-        draggableCard1 = findViewById(R.id.draggableCard1)
-        draggableCard2 = findViewById(R.id.draggableCard2)
-        draggableCard3 = findViewById(R.id.draggableCard3)
-        draggableCard4 = findViewById(R.id.draggableCard4)
-        parentContainer = findViewById(R.id.parentCoordinatorLayout)
 
         // Agregar las vistas arrastrables al DraggableCoordinatorLayout
-        parentContainer.addDraggableChild(draggableCard1)
-        parentContainer.addDraggableChild(draggableCard2)
-        parentContainer.addDraggableChild(draggableCard3)
-        parentContainer.addDraggableChild(draggableCard4)
+        //parentContainer.addDraggableChild(draggableCard1)
+        // parentContainer.addDraggableChild(draggableCard2)
+        //parentContainer.addDraggableChild(draggableCard3)
+        // parentContainer.addDraggableChild(draggableCard4)
 
         // Establecer un ViewDragListener personalizado
-        parentContainer.setViewDragListener(object : DraggableCoordinatorLayout.ViewDragListener {
-            override fun onViewCaptured(view: View, i: Int) {
+        //  parentContainer.setViewDragListener(object : DraggableCoordinatorLayout.ViewDragListener {
+        // override fun onViewCaptured(view: View, i: Int) {
                 // Establecer la bandera isDragged en true para la MaterialCardView correspondiente
-                when (view.id) {
-                    R.id.draggableCard1 -> draggableCard1.isDragged = true
-                    R.id.draggableCard2 -> draggableCard2.isDragged = true
-                    R.id.draggableCard3 -> draggableCard3.isDragged = true
-                    R.id.draggableCard4 -> draggableCard4.isDragged = true
-                }
-            }
+        // when (view.id) {
+        //    R.id.draggableCard1 -> draggableCard1.isDragged = true
+        //   R.id.draggableCard2 -> draggableCard2.isDragged = true
+        //   R.id.draggableCard3 -> draggableCard3.isDragged = true
+        //    R.id.draggableCard4 -> draggableCard4.isDragged = true
+        //  }
+        // }
 
-            override fun onViewReleased(view: View, v: Float, v1: Float) {
+        // override fun onViewReleased(view: View, v: Float, v1: Float) {
                 // Establecer la bandera isDragged en false para la MaterialCardView correspondiente
-                when (view.id) {
-                    R.id.draggableCard1 -> draggableCard1.isDragged = false
-                    R.id.draggableCard2 -> draggableCard2.isDragged = false
-                    R.id.draggableCard3 -> draggableCard3.isDragged = false
-                    R.id.draggableCard4 -> draggableCard4.isDragged = false
-                }
-            }
-        })
+        //     when (view.id) {
+        //     R.id.draggableCard1 -> draggableCard1.isDragged = false
+        //     R.id.draggableCard2 -> draggableCard2.isDragged = false
+        //     R.id.draggableCard3 -> draggableCard3.isDragged = false
+        //     R.id.draggableCard4 -> draggableCard4.isDragged = false
+        //  }
+        // }
+        // })
 
     }
 }
